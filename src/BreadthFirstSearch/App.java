2dbecdac1e0203000650e4dec27b3cffb260cbf2
Juan Letamendia <juan.letamendia@fundkers.com>
2018-05-05 13:31:12 -0300

    added logic to app

--- src/BreadthFirstSearch/App.java
+++ src/BreadthFirstSearch/App.java
@@ -1,8 +1,58 @@
 package BreadthFirstSearch;
 
+
+import java.util.List;
+
 public class App {
 
-	private int data;
 	
+
+//List<Vertex> aList = new ArrayList();
+//
+//
+//var bNode = new Vertex(2);
+//
+//var cNode = new Vertex(3);
+//
+//aList.add(bNode);
+//aList.add(cNode);
+//
+//aNode.setNeighbourList(secondLevelList);
+//
+////b node own list
+//
+//List<Vertex> bList = new ArrayList();
+//
+//var dNode = new Vertex(4);
+//
+//var eNode = new Vertex(5);
+//
+//var fNode = new Vertex(6);
+//
+//bList.add(dNode);
+//
+//bList.add(eNode);
+//
+//bList.add(fNode);
+//
+//bNode.setNeighbourList(bList);
+//
+////c node own list
+//
+//List<Vertex> cList = new ArrayList();
+//
+//var gNode = new Vertex(7);
+//
+//var hNode = new Vertex(8);
+//
+//var iNode = new Vertex(9);
+//
+//cList.add(gNode);
+//
+//cList.add(hNode);
+//
+//cList.add(iNode);
+//
+//cNode.setNeighbourList(cList);
 	
 }
