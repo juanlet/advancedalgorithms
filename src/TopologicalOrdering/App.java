4314a94ef7358a7867668f31a9c667a11519c501
Juan Letamendia <juan.letamendia@fundkers.com>
2018-05-07 01:31:36 -0300

    added topological order algorithm with dfs

---
+++ src/TopologicalOrdering/App.java
@@ -0,0 +1,44 @@
+package TopologicalOrdering;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class App {
+   
+	public static void main(String[] args) {
+		
+		TopologicalOrdering topologicalOrdering = new TopologicalOrdering();
+		
+		List<Vertex> graph = new ArrayList<>();
+		
+		graph.add(new Vertex("0"));
+		graph.add(new Vertex("1"));
+		graph.add(new Vertex("2"));
+		graph.add(new Vertex("3"));
+		graph.add(new Vertex("4"));
+		graph.add(new Vertex("5"));
+		
+		//define dependencies between vertexes
+		graph.get(2).addNeighbour(graph.get(3));
+		graph.get(3).addNeighbour(graph.get(1));
+		graph.get(4).addNeighbour(graph.get(0));
+		graph.get(4).addNeighbour(graph.get(1));
+		graph.get(5).addNeighbour(graph.get(0));
+		graph.get(5).addNeighbour(graph.get(2));
+		
+		for( int i= 0;i<graph.size();++i) {
+			Vertex vertex = graph.get(i);
+			if(!vertex.isVisited())
+				topologicalOrdering.dfs(vertex);
+		}
+		
+		Stack<Vertex> stack = topologicalOrdering.getStack();
+		
+		for(int i=0; i<graph.size();++i) {
+			Vertex vertex = stack.pop();
+			System.out.println(vertex + " -> ");
+		}
+		
+	}
+}
---
+++ src/TopologicalOrdering/Vertex.java
@@ -0,0 +1,50 @@
+package TopologicalOrdering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vertex {
+	private String data;
+	private boolean visited;
+	private List<Vertex> neighbourList;
+
+	public Vertex(String data) {
+		this.data = data;
+		this.neighbourList = new ArrayList<>();
+	}
+
+	public void addNeighbour(Vertex vertex) {
+		this.neighbourList.add(vertex);
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public boolean isVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+	public List<Vertex> getNeighbourList() {
+		return neighbourList;
+	}
+
+	public void setNeighbourList(List<Vertex> neighbourList) {
+		this.neighbourList = neighbourList;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.data;
+	}
+
+}
