89ba102a16d482daa82d2f3621562ad097a6ca12
Juan Letamendia <juan.letamendia@fundkers.com>
2018-05-10 18:08:58 -0300

    Dijkstra algorithm added

---
+++ src/DIjkstraAlgorithm/App.java
@@ -0,0 +1,25 @@
+package DIjkstraAlgorithm;
+
+public class App {
+	
+public static void main(String[] args) {
+	
+	Vertex vertex0 = new Vertex("A");
+	Vertex vertex1 = new Vertex("B");
+	Vertex vertex2 = new Vertex("C");
+	
+	vertex0.addNeighbour(new Edge(1, vertex0,vertex1));
+	vertex0.addNeighbour(new Edge(3, vertex0,vertex2));
+	vertex1.addNeighbour(new Edge(1, vertex1,vertex2));
+
+	DIjkstraAlgorithm algorithm = new DIjkstraAlgorithm();
+	algorithm.computePaths(vertex0);
+	
+	System.out.println(algorithm.getShortestPathTo(vertex2));
+	
+	
+}
+	
+	
+
+}
